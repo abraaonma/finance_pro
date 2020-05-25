86d1947dc573f8b690f2b5c7fd22b6670d141609
abraaonma <abraaonmazevedo@gmail.com>
2020-05-24 22:49:26 -0300

    Projeto.php

---
+++ PROJETO.php
@@ -0,0 +1,29 @@
+<?php
+/*+++++++++++ 
+
+PROJETO QUERO UM APP
+
+BANCO DE DADOS: queroumapp_nomedocomercio
+
+TABELAS DE CADASTROS DE USUARIOS E REGISTROS: 
+
+1. Usuário: -> app_us
+2. Endereço: -> app_end
+3. Forma de pagamento (Débito, Crédito, Transferência) -> app_pag
+4. Registro Financeiro: -> app_regfin
+5. Deletar Registro -> app_excfin
+6. Fechamento Caixa: -> app_cx
+
+TABELA DE CADASTRO DE SERVIÇOS
+
+7. Cadastrar Serviços (menus, cardápio) -> app_cadserv
+
+TABELAS DE ROTINAS
+
+8. Pedido Confirmado -> app_pedconfir
+9. Pedido em Preparação -> app_emprep
+10. Pedido Pronto -> app_pedpront
+11. Pedido a Caminho -> app_acam
+
++++++++++++++*/
+?>
